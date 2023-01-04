f84206271dd6b5b314705b5a127a0d180d1fccd0
Jiangming Liu <jmliunlp@gmail.com>
2023-01-04 16:56:19 +0800

    init with bugs

--- inference.py
+++ inference.py
@@ -1,4 +1,4 @@
-from utils_metrics import get_entities_bio, f1_score, classification_report
+from utils_metrics import metrics_classification 
 from transformers import BartForConditionalGeneration, BartTokenizer, BartConfig
 import torch
 import time
@@ -9,87 +9,83 @@ class InputExample():
         self.words = words
         self.labels = labels
 
-def template_entity(words, input_TXT, start):
-    # input text -> template
-    words_length = len(words)
-    words_length_list = [len(i) for i in words]
-    input_TXT = [input_TXT]*(5*words_length)
+def make_labels(score, label_list):
+   
+    binary_score = score[0:2]
+    multiple_score = score[2:]
 
-    input_ids = tokenizer(input_TXT, return_tensors='pt')['input_ids']
+    label_index = binary_score.index(max(binary_score))
+    binary_label = label_list[label_index]
+
+    label_index = multiple_score.index(max(multiple_score)) + 2
+    multiple_label = label_list[label_index]
+
+    if binary_label == "NO":
+        return [binary_label, "/"]
+    else:
+        return [binary_label, multiple_label]
+
+def prediction(input_TXT):
+
     model.to(device)
-    template_list = [" is a location entity .", " is a person entity .", " is an organization entity .",
-                     " is an other entity .", " is not a named entity ."]
-    entity_dict = {0: 'LOC', 1: 'PER', 2: 'ORG', 3: 'MISC', 4: 'O'}
-    temp_list = []
-    for i in range(words_length):
-        for j in range(len(template_list)):
-            temp_list.append(words[i]+template_list[j])
 
-    output_ids = tokenizer(temp_list, return_tensors='pt', padding=True, truncation=True)['input_ids']
+    template_list = [
+        "It is a straightforward expression",
+        "It is a grammatical metaphorical expression",
+        "It is decorated with interpersonal-mood grammatical metaphor, expressing a offer",
+        "It is decorated with interpersonal-mood grammatical metaphor, expressing a command",
+        "It is decorated with interpersonal-mood grammatical metaphor, expressing a statement",
+        "It is decorated with interpersonal-mood grammatical metaphor, expressing a question",
+        "It is decorated with interpersonal-modality grammatical metaphor, expressing a probability",
+        "It is decorated with interpersonal-modality grammatical metaphor, expressing a usuality",
+        "It is decorated with interpersonal-modality grammatical metaphor, expressing a obligation",
+        "It is decorated with interpersonal-modality grammatical metaphor, expressing a inclination",
+        "It is decorated with ideational grammatical metaphor"
+        ]
+
+    label_list = [
+        "NO",
+        "YES",
+        "interpersonal-mood-offer",
+        "interpersonal-mood-command",
+        "interpersonal-mood-statement",
+        "interpersonal-mood-question",
+        "interpersonal-modality-probability",
+        "interpersonal-modality-usuality",
+        "interpersonal-modality-obligation",
+        "interpersonal-modality-inclination",
+        "ideational"
+        ]
+
+    template_list_length = len(template_list)
+
+    input_TXT = input_TXT.split(' ')
+    input_TXT = [input_TXT] * template_list_length
+    input_ids = tokenizer(input_TXT, return_tensors='pt')['input_ids']
+
+    output_ids = tokenizer(template_list, return_tensors='pt', padding=True, truncation=True)['input_ids']
     output_ids[:, 0] = 2
-    output_length_list = [0]*5*words_length
+     
+    output_length_list = []
+    for i in range(template_list_length):
+        output_length = tokenizer(template_list[i], return_tensors='pt', padding=True, truncation=True)['input_ids'].shape[1] - 2
+        output_length_list.append(output_length)
 
-
-    for i in range(len(temp_list)//5):
-        base_length = ((tokenizer(temp_list[i * 5], return_tensors='pt', padding=True, truncation=True)['input_ids']).shape)[1] - 4
-        output_length_list[i*5:i*5+ 5] = [base_length]*5
-        output_length_list[i*5+4] += 1
-
-    score = [1]*5*words_length
+    score = [1] * template_list_length
     with torch.no_grad():
-        output = model(input_ids=input_ids.to(device), decoder_input_ids=output_ids[:, :output_ids.shape[1] - 2].to(device))[0]
+        output = model(input_ids=input_ids.to(device), decoder_input_ids=output_ids[:, :output_ids.shape[1] - 2].to(device))[0]        
         for i in range(output_ids.shape[1] - 3):
-            # print(input_ids.shape)
             logits = output[:, i, :]
             logits = logits.softmax(dim=1)
-            # values, predictions = logits.topk(1,dim = 1)
             logits = logits.to('cpu').numpy()
-            # print(output_ids[:, i+1].item())
-            for j in range(0, 5*words_length):
+            for j in range(template_list_length):
                 if i < output_length_list[j]:
                     score[j] = score[j] * logits[j][int(output_ids[j][i + 1])]
 
-    end = start+(score.index(max(score))//5)
-        # score_list.append(score)
-    return [start, end, entity_dict[(score.index(max(score))%5)], max(score)] #[start_index,end_index,label,score]
 
+    pred_labels = make_labels(score, label_list)
 
-
-def prediction(input_TXT):
-    input_TXT_list = input_TXT.split(' ')
-
-    entity_list = []
-    for i in range(len(input_TXT_list)):
-        words = []
-        for j in range(1, min(9, len(input_TXT_list) - i + 1)):
-            word = (' ').join(input_TXT_list[i:i+j])
-            words.append(word)
-
-        entity = template_entity(words, input_TXT, i) #[start_index,end_index,label,score]
-        if entity[1] >= len(input_TXT_list):
-            entity[1] = len(input_TXT_list)-1
-        if entity[2] != 'O':
-            entity_list.append(entity)
-    i = 0
-    if len(entity_list) > 1:
-        while i < len(entity_list):
-            j = i+1
-            while j < len(entity_list):
-                if (entity_list[i][1] < entity_list[j][0]) or (entity_list[i][0] > entity_list[j][1]):
-                    j += 1
-                else:
-                    if entity_list[i][3] < entity_list[j][3]:
-                        entity_list[i], entity_list[j] = entity_list[j], entity_list[i]
-                        entity_list.pop(j)
-                    else:
-                        entity_list.pop(j)
-            i += 1
-    label_list = ['O'] * len(input_TXT_list)
-
-    for entity in entity_list:
-        label_list[entity[0]:entity[1]+1] = ["I-"+entity[2]]*(entity[1]-entity[0]+1)
-        label_list[entity[0]] = "B-"+entity[2]
-    return label_list
+    return pred_labels
 
 def cal_time(since):
     now = time.time()
@@ -99,66 +95,43 @@ def cal_time(since):
     return '%dm %ds' % (m, s)
 
 
-tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
+tokenizer = BartTokenizer.from_pretrained('facebook/bart-base')
 # input_TXT = "Japan began the defence of their Asian Cup title with a lucky 2-1 win against Syria in a Group C championship match on Friday ."
-model = BartForConditionalGeneration.from_pretrained('./checkpoint-3060')
+#model = BartForConditionalGeneration.from_pretrained('./checkpoint-3060')
+model = BartForConditionalGeneration.from_pretrained('./outputs/best_model/')
 # model = BartForConditionalGeneration.from_pretrained('../dialogue/bart-large')
 model.eval()
 model.config.use_cache = False
 # input_ids = tokenizer(input_TXT, return_tensors='pt')['input_ids']
 # print(input_ids)
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-score_list = []
-file_path = './conll2003/test.txt'
-guid_index = 1
+input_file = './conll2003/test.txt'
+output_file = ''
 examples = []
-with open(file_path, "r", encoding="utf-8") as f:
-    words = []
-    labels = []
-    for line in f:
-        if line.startswith("-DOCSTART-") or line == "" or line == "\n":
-            if words:
-                examples.append(InputExample(words=words, labels=labels))
-                words = []
-                labels = []
-        else:
-            splits = line.split(" ")
-            words.append(splits[0])
-            if len(splits) > 1:
-                labels.append(splits[-1].replace("\n", ""))
-            else:
-                # Examples could have no label for mode = "test"
-                labels.append("O")
-    if words:
-        examples.append(InputExample(words=words, labels=labels))
+for line in open(file_path):
+    line = line.strip()
+    if line == "":
+        continue
+    words, labels, targets = line.split("\t")
+    InputExample(words=words, labels=labels)
+    examples.append(InputExample)    
 
 trues_list = []
 preds_list = []
-str = ' '
 num_01 = len(examples)
 num_point = 0
 start = time.time()
 for example in examples:
-    sources = str.join(example.words)
+    sources = example.words
     preds_list.append(prediction(sources))
     trues_list.append(example.labels)
     print('%d/%d (%s)'%(num_point+1, num_01, cal_time(start)))
-    print('Pred:', preds_list[num_point])
-    print('Gold:', trues_list[num_point])
     num_point += 1
 
+with open(output_file, "w") as f:
+    for a, b in zip(trues_list, preds_list):
+        f.write("{} | {}\n".format(" ".join(a), " ".join(b)))
+    f.close()
 
-true_entities = get_entities_bio(trues_list)
-pred_entities = get_entities_bio(preds_list)
-results = {
-    "f1": f1_score(true_entities, pred_entities)
-}
-print(results["f1"])
-for num_point in range(len(preds_list)):
-    preds_list[num_point] = ' '.join(preds_list[num_point]) + '\n'
-    trues_list[num_point] = ' '.join(trues_list[num_point]) + '\n'
-with open('./pred.txt', 'w') as f0:
-    f0.writelines(preds_list)
-with open('./gold.txt', 'w') as f0:
-    f0.writelines(trues_list)
+scores = metrics_classification(trues_list, preds_list)
